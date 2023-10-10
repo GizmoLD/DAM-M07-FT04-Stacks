24dae1a81520687ff159091bf2577be706f92aaf
GizmoLD <122391569+GizmoLD@users.noreply.github.com>
2023-10-10 19:13:01 +0200

    1

---
+++ stacks/lib/app_data.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppData with ChangeNotifier {
+// App status
+  bool isSidebarLeftVisible = false;
+
+// Toggle sidebar left
+  void toggleSidebarLeft() {
+    isSidebarLeftVisible = !isSidebarLeftVisible;
+    notifyListeners();
+  }
+}
